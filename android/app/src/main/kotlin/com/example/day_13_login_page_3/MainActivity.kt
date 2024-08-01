d26746b6c00c10bbb82f7da56f87a10d1a1b470c
VIA2NEY <kouameangeV11@gmail.com>
2024-08-01 18:55:27 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/day_13_login_page_3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day_13_login_page_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:day_13_login_page_3/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
